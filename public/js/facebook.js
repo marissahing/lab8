1c36fdf9136e4d6f44dd221c36a4bbe0b391cb7b
Marissa Hing <mnhing@ucsd.edu>
2017-03-02 02:27:40 -0800

    Third party API's

---
+++ public/js/facebook.js
@@ -0,0 +1,30 @@
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+function statusChangeCallback(response) {
+  console.log('Facebook login status changed.');
+  console.log(response);
+  // The response object is returned with a status field that lets the
+  // app know the current login status of the person.
+  // Full docs on the response object can be found in the documentation
+  // for FB.getLoginStatus().
+  if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    FB.api('/me?fields=name,first_name,picture.width(480)', changeUser);
+  }
+}
+
+function changeUser(response) { 
+  console.log(response);
+  $('.facebookLogin').hide();
+  var name = '<h1>' + response['name'] + '</h1>';
+  $('#name').html(name);
+  var img = response['picture'];
+  var dataImg = img['data'];
+  var imgURL = dataImg['url']
+  $('#photo').attr('src', imgURL);
+}
+
